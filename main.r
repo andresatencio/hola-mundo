a62799d4c609128a8bae912635f8ac836c23b1eb
Atencio <andres.atencio@edensa.com.ar>
2018-10-12 13:30:57 -0300

    :cat:

---
+++ main.r
@@ -0,0 +1,24 @@
+# Primer ejemplo, veremos que onda
+
+# Asignar variables. Enteros y string
+edad <- 20
+edad
+nombre <- "Andres"
+nombre
+30 -> edad
+edad
+"Fruta" -> nombre
+nombre
+apellido <- "Sarlanga"
+nombreCompleto <- paste(nombre, apellido)
+nombreCompleto
+
+# Muestra variables en memoria 
+ls.str()
+
+# funcion
+saludo <- function () {
+    print('Hola 🐈')
+}
+
+saludo()
