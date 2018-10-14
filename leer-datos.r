9d7382d187dd7189573f4c6a355570b76c5cd8cb
Atencio <andres.atencio@edensa.com.ar>
2018-10-14 12:28:48 -0300

    :fire:

---
+++ leer-datos.r
@@ -0,0 +1,6 @@
+
+# No encontre como hacer para que use path relativo
+path <- 'C:\\Users\\😎\\Documents\\Curso R\\code\\hola-mundo\\'
+hawaii <- read.csv(paste(path, 'datolos.csv'))
+head(hawaii)
+
---
+++ vectores.r
@@ -0,0 +1,22 @@
+# Algo de vectores
+
+arr1 <- c(1,2,3,4,5)
+print(arr)
+
+arr2 <- c(arr1, 6,7,8)
+print(arr2)
+
+arr3 <- c(arr1, arr2)
+print(arr3)
+
+secuencia <- seq(0, 10)
+print(secuencia)
+
+secuencia <- seq(0, 10, by=3)
+print(secuencia)
+
+arrString <- c('Hola', 'todo piola', '?')
+print(arrString)
+
+arr4 <- c(1,1,1,1,1,1,1,3,3,3,3,3,3)
+print(unique(arr4))
