6772b3743006dfc88379971391313fbdda02e40d
Atencio <andres.atencio@edensa.com.ar>
2018-10-15 15:19:19 -0300

    :smile: extraer columna

--- leer-datos.r
+++ leer-datos.r
@@ -1,6 +1,10 @@
 
 # No encontre como hacer para que use path relativo
-path <- 'C:\\Users\\😎\\Documents\\Curso R\\code\\hola-mundo\\'
+path <- 'C:\\Users\\[usuario]\\Documents\\Curso R\\code\\hola-mundo\\'
 hawaii <- read.csv(paste(path, 'datolos.csv'))
 head(hawaii)
-
+# traer una columna
+nombres <- hawaii[,2]
+print(nombres)
+# Muestra variables en memoria 
+ls.str()
